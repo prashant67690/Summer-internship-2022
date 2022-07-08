205fe01dde33ed7bd2119f1c6e6ae5171b94c162
prashant kumar <80661803+prashant67690@users.noreply.github.com>
2022-07-08 12:31:12 +0530

    Update index.js

--- day 4 js-assignment/sixth Question/index.js
+++ day 4 js-assignment/sixth Question/index.js
@@ -1,12 +1,14 @@
+
+
 const gradeStudent = (studentRecord) => {
-    const isAGrade = studentRecord.totalMarks >= 90 ? true : false;
-    const checkCriteria = studentRecord.examType === "Final exams" && isAGrade;
-    return checkCriteria;
-  };
-  
-  const studentRecord = {
-    totalMarks: 85,
-    examType: "term-1 exams",
-  };
-  
-  console.log(gradeStudent(studentRecord));
+  const isAGrade = studentRecord.totalMarks >= 90 ? true : false;
+  const checkCriteria = studentRecord.examType === "Final exams" && isAGrade;
+  return checkCriteria;
+};
+
+const studentRecord = {
+  totalMarks: 85,
+  examType: "term-1 exams",
+};
+
+console.log(gradeStudent(studentRecord));
